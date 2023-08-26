bd65e684b9a9a47444e628d970bdf9a6938c1e38
RamonaMonica <136003335+RamonaMonica@users.noreply.github.com>
2023-08-26 14:26:19 +0000

    Deploying to gh-pages from @ RamonaMonica/SIMPLY-CHOCOLATE-TEAM8@02b8c7893246099acfa58d19f7e5e9ceeb7fd843 🚀

---
+++ index.52df4a69.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[subscribe-data-modal-open]"),closeModalBtn:document.querySelector("[subscribe-data-modal-close]"),modal:document.querySelector("[subscribe-data-modal]")};function o(){e.modal.classList.toggle("subscribe-is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.52df4a69.js.map
